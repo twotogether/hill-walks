752a8c1d35b1b68588fd39896fe833c8770a6148
Mohana Das <dasmohana91@gmail.com>
2026-04-21 11:45:49 +0100

    Add CSS file to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_css_files = ['gallery.css']
 html_theme_options = {
     "sidebar_hide_secondary": False,
 }
